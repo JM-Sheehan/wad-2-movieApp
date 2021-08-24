cfc8331b60492aa1409cbd79b2ff145821d05b22
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-08-24 12:46:27 +0100

    Final Commit

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -68,4 +68,19 @@ export const getMovies = () => {
     else{
       return "male";
     }
-  }
+  };
+
+  export const newGuestSession = () =>{
+    return fetch(
+      `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then(res => res.json());
+  };
+
+  export const addRating = (movie_id, rating, guest_id) =>{
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${movie_id}/
+      ${rating}?api_key=${process.env.REACT_APP_TMDB_KEY}
+      &guest_session_id=${guest_id}`
+    )
+    .then(res => res.json());
+  };
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,6 +3,7 @@ import PageTemplate from '../components/templateMovieListPage'
 import {MoviesContext} from '../contexts/moviesContext'
 import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
+
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
   const movies = context.movies.filter((m) => {  // New
